57cfc794565649bc93506314fb408d981ab78400
Saurav Asopa <asopasaurav88@gmail.com>
2025-08-06 22:52:44 +0530

    Initial commit

---
+++ src/main/java/com/example/raj_tutorial/raj_tutorial/RajTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.raj_tutorial.raj_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajTutorialApplication.class, args);
+	}
+
+}
